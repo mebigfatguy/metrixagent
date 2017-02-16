73377b2644f8b973e56a2a86229c6c45639d479c
Dave Brosius <dbrosius@mebigfatguy.com>
2017-02-15 19:18:44 -0500

    attempt to duplicate what javac and replicate the finally block before all returns

--- src/main/java/com/mebigfatguy/metrixagent/MetrixAgentMethodVisitor.java
+++ src/main/java/com/mebigfatguy/metrixagent/MetrixAgentMethodVisitor.java
@@ -17,50 +17,156 @@
  */
 package com.mebigfatguy.metrixagent;
 
+import java.util.BitSet;
+
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.commons.LocalVariablesSorter;
+import org.objectweb.asm.commons.Method;
 
 public class MetrixAgentMethodVisitor extends LocalVariablesSorter {
 
-    private Label startLabel;
-    private Label endLabel;
+    private static final BitSet returnOps = new BitSet();
+
+    private String methodDesc;
+    private Label tryLabel;
+    private Label tryEndLabel;
     private Label handlerLabel;
+    private Label procEndLabel;
     private String fqMethod;
     private int startTimeReg;
+    private Type returnType;
+    private int returnValReg;
+    private int returnOp;
+
+    static {
+        returnOps.set(Opcodes.RETURN);
+        returnOps.set(Opcodes.ARETURN);
+        returnOps.set(Opcodes.IRETURN);
+        returnOps.set(Opcodes.LRETURN);
+        returnOps.set(Opcodes.FRETURN);
+        returnOps.set(Opcodes.DRETURN);
+    }
 
     public MetrixAgentMethodVisitor(MethodVisitor mv, int access, String desc, String fullyQualifiedMethod) {
         super(Opcodes.ASM5, access, desc, mv);
         fqMethod = fullyQualifiedMethod;
+        methodDesc = desc;
     }
 
     @Override
     public void visitCode() {
-        startLabel = new Label();
-        endLabel = new Label();
+        Label procStartLabel = new Label();
+        procEndLabel = new Label();
+        tryLabel = new Label();
+        tryEndLabel = new Label();
         handlerLabel = new Label();
 
         startTimeReg = super.newLocal(Type.LONG_TYPE);
+        super.visitLocalVariable("$startTime", "J", null, procStartLabel, procEndLabel, startTimeReg);
 
-        super.visitLabel(startLabel);
-        super.visitTryCatchBlock(startLabel, endLabel, handlerLabel, null);
+        returnType = getReturnType(methodDesc);
+        if (returnType != Type.VOID_TYPE) {
+            returnValReg = super.newLocal(returnType);
+            super.visitLocalVariable("$returnVal", returnType.getDescriptor(), null, procStartLabel, procEndLabel, returnValReg);
+        }
+
+        super.visitLabel(tryLabel);
+        super.visitTryCatchBlock(tryLabel, tryEndLabel, handlerLabel, null);
         super.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
         super.visitVarInsn(Opcodes.LSTORE, startTimeReg);
         super.visitCode();
     }
 
     @Override
+    public void visitInsn(int opcode) {
+
+        if (returnOps.get(opcode)) {
+            returnOp = opcode;
+            if (returnType != Type.VOID_TYPE) {
+                switch (returnType.getSort()) {
+                    case Type.OBJECT:
+                        visitVarInsn(Opcodes.ASTORE, returnValReg);
+                    break;
+
+                    case Type.INT:
+                        visitVarInsn(Opcodes.ISTORE, returnValReg);
+                    break;
+
+                    case Type.LONG:
+                        visitVarInsn(Opcodes.LSTORE, returnValReg);
+                    break;
+
+                    case Type.FLOAT:
+                        visitVarInsn(Opcodes.FSTORE, returnValReg);
+                    break;
+
+                    case Type.DOUBLE:
+                        visitVarInsn(Opcodes.DSTORE, returnValReg);
+                    break;
+                }
+            }
+
+            injectExitCode();
+
+            injectPushReturnValueOnStack();
+        }
+        super.visitInsn(opcode);
+    }
+
+    @Override
     public void visitMaxs(int maxStack, int maxLocals) {
-        super.visitLabel(endLabel);
+        super.visitLabel(tryEndLabel);
         super.visitLabel(handlerLabel);
+        super.visitInsn(Opcodes.POP);
+        injectExitCode();
+        if (returnOp != Opcodes.RETURN) {
+            injectPushReturnValueOnStack();
+        }
+        super.visitInsn(returnOp);
+        super.visitLabel(procEndLabel);
+        super.visitMaxs(maxStack, maxLocals);
+    }
+
+    private void injectExitCode() {
         super.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
         super.visitVarInsn(Opcodes.LLOAD, startTimeReg);
         super.visitInsn(Opcodes.LSUB);
         super.visitLdcInsn(fqMethod);
         super.visitMethodInsn(Opcodes.INVOKESTATIC, MetrixAgentRecorder.class.getName().replace('.', '/'), "record", "(JLjava/lang/String;)Z", false);
-        super.visitMaxs(maxStack, maxLocals);
+    }
+
+    private void injectPushReturnValueOnStack() {
+        if (returnType != Type.VOID_TYPE) {
+            switch (returnType.getSort()) {
+                case Type.OBJECT:
+                    visitVarInsn(Opcodes.ALOAD, returnValReg);
+                break;
+
+                case Type.INT:
+                    visitVarInsn(Opcodes.ILOAD, returnValReg);
+                break;
+
+                case Type.LONG:
+                    visitVarInsn(Opcodes.LLOAD, returnValReg);
+                break;
+
+                case Type.FLOAT:
+                    visitVarInsn(Opcodes.FLOAD, returnValReg);
+                break;
+
+                case Type.DOUBLE:
+                    visitVarInsn(Opcodes.DLOAD, returnValReg);
+                break;
+            }
+        }
+    }
+
+    private Type getReturnType(String desc) {
+        Method m = new Method("x", desc);
+        return m.getReturnType();
     }
 
 }
